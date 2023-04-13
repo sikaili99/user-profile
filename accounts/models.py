ae9f39310bc76a938badda4777e2fa4c9c5bd31b
sikaili99 <sikaili99@gmail.com>
2023-04-14 00:43:40 +0200

    Created profile model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.gis.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    home_address = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20)
+    location = models.PointField(blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
