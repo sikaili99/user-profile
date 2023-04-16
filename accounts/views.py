795372d7778796fa9a51735e97736aee048284df
sikaili99 <sikaili99@gmail.com>
2023-04-16 15:31:14 +0200

    created signup, login and logout views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate, logout
+from accounts.forms import SignInForm, SignUpForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
-# Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('accounts:login')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
+
+
+def signin(request):
+    if request.method == 'POST':
+        form = SignInForm(request.POST)
+        print(form.is_valid())
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            print(username, password)
+            user = authenticate(username=username, password=password)
+            print(user)
+            if user is not None:
+                login(request, user)
+                return redirect('accounts:profile')
+            else:
+                messages.error(request, 'Invalid username or password.')
+    else:
+        form = SignInForm()
+    return render(request, 'login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('accounts:login')
