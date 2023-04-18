524808ae42f155b3210ccff4e5722e435107eabb
sikaili99 <sikaili99@gmail.com>
2023-04-18 15:48:42 +0200

    view markers on map and close on clicking new mark

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
-from accounts.models import Profile
+from accounts.models import Profile, CustomUser
 
 
 class SignUpForm(UserCreationForm):
@@ -13,7 +12,7 @@ class SignUpForm(UserCreationForm):
         max_length=254, help_text='Required. Inform a valid email address.')
 
     class Meta:
-        model = User
+        model = CustomUser
         fields = ('username', 'first_name', 'last_name',
                   'email', 'password1', 'password2', )
 
