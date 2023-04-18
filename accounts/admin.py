925e31c69dd18b8de92d0fe4f88e795ab8afb8f9
sikaili99 <sikaili99@gmail.com>
2023-04-18 15:45:07 +0200

    added custom admin model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,24 @@
 from django.contrib import admin
-from .models import Profile
+from .models import CustomUser, Profile
 
-admin.site.register(Profile)
+
+class ProfileModelAdmin(admin.ModelAdmin):
+    """
+    Show logged in super user their account only. Only super user can view all accounts
+    """
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+
+        if request.user.is_superuser:
+            return qs.all()
+        else:
+            # FieldError then query for user to get profiles
+            try:
+                return qs.filter(profile__user=request.user)
+            except:
+                return qs.filter(user=request.user)
+
+
+admin.site.register(Profile, ProfileModelAdmin)
+admin.site.register(CustomUser, ProfileModelAdmin)
